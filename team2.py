7ad8dd1245e756d0c624e6c77832c9239e2fc410
devinbaker <19dbaker31@isd77.k12.mn.us>
2017-12-04 14:59:39 -0600

    Strategy
    Strategy for Code

--- team2.py
+++ team2.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'Team Devin' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Collude but retaliate'
+strategy_description = 'Collude first round. Collude, except in a round after getting a severe punishment.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,7 +17,11 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    if len(my_history)==0: # It's the first round; collude.
+        return 'c'
+    elif my_history[-1]=='c' and their_history[-1]=='b':
+        return 'b' # Betray if they were severely punished last time,
+    else:
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -26,7 +30,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+        return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
